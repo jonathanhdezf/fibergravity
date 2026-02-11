fe6af9d90da9baeb223547c356a7c947d6029494
jonathanhdezf <jonathanhdezf@gmail.com>
2026-02-11 16:42:41 -0600

    feat: add blog link to navbar

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -23,6 +23,7 @@ const navLinks = [
     { name: "TV", href: "#television", icon: <Tv className="w-4 h-4" /> },
     { name: "Medidor", href: "#speed-test", icon: <Gauge className="w-4 h-4" /> },
     { name: "Cobertura", href: "#cobertura", icon: <Radar className="w-4 h-4" /> },
+    { name: "Blog", href: "/blog", icon: <Wifi className="w-4 h-4" /> },
     { name: "Partners", href: "#socios", icon: <Users className="w-4 h-4" /> },
 ];
 
